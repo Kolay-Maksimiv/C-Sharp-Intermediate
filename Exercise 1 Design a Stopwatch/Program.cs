cf30067851abc770962c31a8d4d10c807d4a2105
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2022-03-06 03:45:21 +0200

    Exercise 1 Design a Stopwatch

---
+++ Exercise 1 Design a Stopwatch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise_1_Design_a_Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine(stopwatch.Stop());
+        }
+    }
+}
---
+++ Exercise 1 Design a Stopwatch/Stopwatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_1_Design_a_Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime Seconds { get; private set; }
+
+        public void Start()
+        {
+            Seconds = DateTime.Now;
+        }
+
+        public double Stop()
+        {
+            var timeSpan = DateTime.Now - Seconds;
+            var seconds = timeSpan.TotalSeconds;
+
+            return seconds;
+        }
+
+    }
+}
